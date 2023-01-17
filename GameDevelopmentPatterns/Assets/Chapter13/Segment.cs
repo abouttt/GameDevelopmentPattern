16f5ce123edd329065fc22cf6d316ff5164d3c50
abouttt <abouttt57@gmail.com>
2023-01-17 12:32:31 +0900

    공간 분할 공부.

---
+++ GameDevelopmentPatterns/Assets/Chapter13/Segment.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chapter.SpatialPartition
+{
+    public class Segment : MonoBehaviour
+    {
+        public TrackController trackController;
+        private void OnDestroy()
+        {
+            if (trackController)
+            {
+                trackController.LoadNextSegment();
+            }
+        }
+    }
+}
